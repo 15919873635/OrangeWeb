9c967d523ec6a69b2466fba4b6b3119cc7ddef2d
lining <15919873635@126.com>
2017-01-23 11:00:35 +0800

    快速迭代123

---
+++ src/com/orange/web/parser/PathVariableParser.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.orange.web.parser;
+
+/**
+ *
+ * @author lining
+ */
+public class PathVariableParser extends AbstractAnnotationParser{
+
+    @Override
+    public void parse() {
+        
+    }
+}
