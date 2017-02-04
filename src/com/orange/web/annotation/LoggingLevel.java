dda53c16e3b98eea826cd788d6b5584b67e05757
lining <15919873635@126.com>
2017-02-04 15:39:57 +0800

    快速迭代123

--- src/com/orange/web/annotation/LoggingLevel.java
+++ src/com/orange/web/annotation/LoggingLevel.java
@@ -10,5 +10,12 @@ package com.orange.web.annotation;
  * @author lining
  */
 public enum LoggingLevel {
-    DEBUG,INFO,ERROR
+    /**
+     * DEBUG Level表明细粒度信息事件对调试应用程序是非常有帮助的
+     * INFO level表明消息在粗粒度级别上突出强调应用程序的运行过程
+     * WARN level表明会出现潜在错误的情形
+     * ERROR level指出虽然发生错误事件，但仍然不影响系统的继续运行
+     * FATAL level指出每个严重的错误事件将会导致应用程序的退出
+     */
+    DEBUG,INFO,WARN,ERROR,FATAL
 }
