4cc7f14718fa796dcd6adc79c0e921f4f2c09617
15919873635 <15919873635@126.com>
2017-02-28 16:04:59 +0800

    新增asm class类生成模块

---
+++ src/com/orange/web/classes/generate/TypeGenerateTask.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.orange.web.classes.generate;
+
+import java.util.concurrent.Callable;
+
+/**
+ * 使用多线程进行文件类的生成
+ * @author lining
+ */
+public class TypeGenerateTask implements Callable{
+
+    @Override
+    public Object call() throws Exception {
+        return null;
+    }
+}
